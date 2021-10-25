120baa50c027c1409a530c94ac3891e39fe0b180
QemerS <tu201806169@code.edu.az>
2021-10-26 03:00:18 +0400

    Add project files.

---
+++ DorduncuApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DorduncuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program in C# to display the first 10 natural numbers
+            for (short x = 1; x <= 10; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
